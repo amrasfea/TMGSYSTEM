97829604469649ad2d638b92cc94c89feafdf14e
ffakhira <fakhiraalysa03@gmail.com>
2024-05-31 23:52:35 +0800

    j

--- routes/web.php
+++ routes/web.php
@@ -119,14 +119,17 @@ Route::get('/PlatinumThesisTitle',[ManageWeeklyFocusController::class, 'Platinum
 Route::get('/DTAReport',[ManageWeeklyFocusController::class, 'GenerateDraftThesisPerformanceReport']);
 
 //ManagePublication
-Route::middleware(['auth'])->group(function () {
-    Route::get('/publications', [ManagePublicationController::class, 'index'])->name('platinum.OwnPublicationView');
-    Route::get('/publications/create', [ManagePublicationController::class, 'create'])->name('platinum.AddPublicationView');
-    Route::post('/publications', [ManagePublicationController::class, 'store'])->name('platinum.storePublication');
-    Route::get('/publications/{id}/edit', [ManagePublicationController::class, 'edit'])->name('platinum.EditPublicationView');
-    Route::put('/publications/{id}', [ManagePublicationController::class, 'update'])->name('platinum.updatePublication');
-    Route::delete('/publications/{id}', [ManagePublicationController::class, 'destroy'])->name('platinum.deletePublication');
-});
+Route::get('ViewExpertDomainPublication',[ManagePublicationController::class,'ExpertDomainPublicationView']);
+Route::get('ViewPlatinumPublication',[ManagePublicationController::class,'PlatinumPublicationView']);
+Route::get('ViewPublicationReport',[ManagePublicationController::class,'PublicationReportView']);
+Route::get('ViewRegisteredPlatinum',[ManagePublicationController::class,'RegisteredPlatinumView']);
+Route::get('SearchForPlatinum',[ManagePublicationController::class,'SearchPlatinumVIew']);
+
+Route::get('AddPublication',[ManagePublicationController::class,'AddPublicationView']);
+Route::post('EditPublication',[ManagePublicationController::class,'EditPublicationView']);
+Route::get('ViewOwnPublication/{id}',[ManagePublicationController::class,'OwnPublicationView']);
+Route::post('SearchForPublication/{id}',[ManagePublicationController::class,'SearchPublicationView']);
+Route::get('ViewOtherPublication',[ManagePublicationController::class,'ViewOtherPublicationView']);
 
 
 
