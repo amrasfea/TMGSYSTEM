85f4c65dff8c9f11dd904451aec64e4c12b4888f
ffakhira <fakhiraalysa03@gmail.com>
2024-06-06 23:29:36 +0800

    xsx

--- resources/views/layouts/platinum-layout.blade.php
+++ resources/views/layouts/platinum-layout.blade.php
@@ -237,10 +237,10 @@
                             <a href="{{ route('publications.index') }}" class="sidebar-link">My Publication</a>
                         </li>
                         <li class="sidebar-item">
-                            <a href="{{ route('publications.platinum') }}" class="sidebar-link">Platinum Publications</a>
+                            <a href="#" class="sidebar-link">Platinum Publications</a>
                         </li>
                         <li class="sidebar-item">
-                            <a href="{{ route('publications.search') }}" class="sidebar-link">Search Publications</a>
+                            <a href="#" class="sidebar-link">Search Publications</a>
                         </li>
 
                     </ul>
