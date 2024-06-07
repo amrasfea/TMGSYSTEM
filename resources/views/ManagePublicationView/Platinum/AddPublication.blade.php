8aeb6362883822223cd85b10562c15f4d859b7ad
ffakhira <fakhiraalysa03@gmail.com>
2024-06-07 19:32:10 +0800

    cc

--- resources/views/ManagePublicationView/Platinum/AddPublication.blade.php
+++ resources/views/ManagePublicationView/Platinum/AddPublication.blade.php
@@ -1,6 +1,12 @@
 <x-platinum-layout>
 
-@section('title', 'Add Publication')
+<x-slot name="header">
+    <h2 class="font-semibold text -xl text-gray-800 leading-tight">
+        {{ __('Add Publication') }}
+    </h2>
+    
+</x-slot>
+
 
 @section('content')
 <div class="container">
