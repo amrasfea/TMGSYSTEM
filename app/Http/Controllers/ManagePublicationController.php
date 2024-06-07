0d9022ea7f761e02ae1eb3cea3dcf7e7309c8965
ffakhira <fakhiraalysa03@gmail.com>
2024-06-07 18:29:54 +0800

    x

--- app/Http/Controllers/ManagePublicationController.php
+++ app/Http/Controllers/ManagePublicationController.php
@@ -11,7 +11,7 @@ class ManagePublicationController extends Controller
 {
     public function __construct()
     {
-        $this->middleware('auth');
+        
     }
 
     public function index()
